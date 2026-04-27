810b83670f6e2d752431b219e4b2260d80b4d566
serj <13497925+gamedevserj@users.noreply.github.com>
2026-04-27 18:05:46 +0400

    Added documentation comments for PopupController

--- PopupSystem/Controllers/PopupController.cs
+++ PopupSystem/Controllers/PopupController.cs
@@ -4,25 +4,42 @@ using UISystem.Core.Views;
 
 namespace UISystem.Core.PopupSystem
 {
+    /// <summary>
+    /// Popup controller.
+    /// </summary>
+    /// <typeparam name="TViewCreator">Type of view creator. Must implement <see cref="IViewCreator{TView}"/>.</typeparam>
+    /// <typeparam name="TView">Type of view. Must implement <see cref="IPopupView"/>.</typeparam>
+    /// <typeparam name="TResult">Type of popup result. Must be of <see cref="Enum"/>.</typeparam>
     internal abstract partial class PopupController<TViewCreator, TView, TResult>
         : Controller<TViewCreator, TView>, IPopupController<TResult>
         where TViewCreator : IViewCreator<TView>
         where TView : IPopupView
         where TResult : Enum
     {
+        private Action<TResult> _onHideAction;
 
-        protected Action<TResult> _onHideAction;
-
-        protected readonly IPopupsManager<TResult> _popupsManager;
-
-        public abstract TResult PressedReturnPopupResult { get; }
-
-        public PopupController(TViewCreator viewCreator, IPopupsManager<TResult> popupsManager)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PopupController{TViewCreator, TView, TResult}"/> class.
+        /// </summary>
+        /// <param name="viewCreator">View creator.</param>
+        /// <param name="popupsManager">Popups manager.</param>
+        protected PopupController(TViewCreator viewCreator, IPopupsManager<TResult> popupsManager)
         {
             ViewCreator = viewCreator;
-            _popupsManager = popupsManager;
+            PopupsManager = popupsManager;
         }
 
+        /// <summary>
+        /// Gets result chosen if pressed return button.
+        /// </summary>
+        public abstract TResult PressedReturnPopupResult { get; }
+
+        /// <summary>
+        /// Gets the popups manager.
+        /// </summary>
+        protected IPopupsManager<TResult> PopupsManager { get; private set; }
+
+        /// <inheritdoc/>
         public override void Init()
         {
             if (!ViewCreator.IsViewValid)
@@ -32,6 +49,7 @@ namespace UISystem.Core.PopupSystem
             }
         }
 
+        /// <inheritdoc/>
         public async Task Show(string message, Action<TResult> onHideAction, bool instant = false)
         {
             CanReceivePhysicalInput = false;
@@ -42,6 +60,7 @@ namespace UISystem.Core.PopupSystem
             View.FocusElement();
         }
 
+        /// <inheritdoc/>
         public async Task Hide(TResult result, bool instant = false)
         {
             CanReceivePhysicalInput = false;
@@ -49,12 +68,14 @@ namespace UISystem.Core.PopupSystem
             _onHideAction?.Invoke(result);
             DestroyView();
         }
-        protected override void DestroyView() => ViewCreator.DestroyView();
 
+        /// <inheritdoc/>
         public override void OnReturnButtonDown()
         {
-            _popupsManager.HidePopup(PressedReturnPopupResult);
+            PopupsManager.HidePopup(PressedReturnPopupResult);
         }
 
+        /// <inheritdoc/>
+        protected override void DestroyView() => ViewCreator.DestroyView();
     }
 }
