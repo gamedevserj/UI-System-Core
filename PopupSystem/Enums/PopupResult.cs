d8cbe913408f8aab2010a737825b3e904f0f9ff3
serj <13497925+gamedevserj@users.noreply.github.com>
2026-05-01 12:50:10 +0400

    Namespace braces

--- PopupSystem/Enums/PopupResult.cs
+++ PopupSystem/Enums/PopupResult.cs
@@ -1,11 +1,12 @@
-﻿namespace UISystem.Core.PopupSystem;
-
-/// <summary>
-/// Defines popup closing results.
-/// </summary>
-public enum PopupResult
+﻿namespace UISystem.Core.PopupSystem
 {
-    No = 0,
-    Yes = 1,
-    Cancel = 2,
+    /// <summary>
+    /// Defines popup closing results.
+    /// </summary>
+    public enum PopupResult
+    {
+        No = 0,
+        Yes = 1,
+        Cancel = 2,
+    }
 }
