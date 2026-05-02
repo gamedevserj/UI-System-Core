8506a756ae8089f29dd65a50109d99fa91ca61a5
Serhii Bohdan <serjio.developer@gmail.com>
2026-05-02 14:51:43 +0400

    Set IFocusableUiElement to be partial, removed methods.

--- Elements/IFocusableUiElement.cs
+++ Elements/IFocusableUiElement.cs
@@ -1,27 +1,10 @@
 ﻿namespace UISystem.Core.Elements
 {
-
     /// <summary>
     /// Defines contract for focusable UI element.
+    /// Godot and Unity have different setup for focusability, so their methods are defined in they own repos.
     /// </summary>
-    public interface IFocusableUiElement
+    public partial interface IFocusableUiElement
     {
-        /// <summary>
-        /// Switches element focus.
-        /// </summary>
-        /// <param name="focus">Whether element should have focus.</param>
-        void SwitchFocus(bool focus);
-
-        /// <summary>
-        /// Checks whether this element is valid.
-        /// </summary>
-        /// <returns>True if element is valid, otherwise - false.</returns>
-        bool IsValidElement();
-
-        /// <summary>
-        /// Switches elements focus mode and mouse filter.
-        /// </summary>
-        /// <param name="focusable">Whether element should be allowed to have focus/be interactable.</param>
-        void SwitchFocusAvailability(bool focusable);
     }
 }
